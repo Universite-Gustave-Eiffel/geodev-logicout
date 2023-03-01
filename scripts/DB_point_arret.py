efa323e34b60fd42df362dd36d74fcb91fcec3b1
Claire_GRD <claire-girardin@hotmail.fr>
2023-03-01 14:00:06 +0100

    création de DB_point_arret

---
+++ scripts/DB_point_arret.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+# Lecture du fichier point_arret.csv
+
+
+
+
+# récupérer par id_simulation (i.e. par trajet) toutes les coordonnées des points d'arrêts dans l'ordre (i.e. par le paramètre index)
+
+
