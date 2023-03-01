1976bc36209dcfc98066ee0704a3ef7b2a0e64ca
Sivloc <clovis.bergeret@laposte.net>
2023-03-01 14:15:42 +0100

    début de l'algo_mutu

--- scripts/algo_mutu.py
+++ scripts/algo_mutu.py
@@ -3,3 +3,14 @@ import pandas as pd
 import yaml
 
 config = yaml.safe_load(open('./config.yml'))
+rayon = config['rayon']
+tuple = config['tuple']
+url = config['url']
+
+def is_mutu(ArrayA,ArrayB):
+    '''
+    Takes two arrays, one for a producer A, one for a producer B, and returns a boolean 
+    
+    
+    
+    '''
