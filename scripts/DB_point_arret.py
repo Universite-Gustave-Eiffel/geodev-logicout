2f4facb99d44d79b4c31ef9416c166a7364781c1
Claire_GRD <claire-girardin@hotmail.fr>
2023-03-01 18:42:35 +0100

    création DB_point_arret

--- scripts/DB_point_arret.py
+++ scripts/DB_point_arret.py
@@ -1,11 +1,29 @@
 import numpy as np
+import csv
 
 
-# Lecture du fichier point_arret.csv
+# Fichier point_arret.csv sous forme d'Array (en retirant l'entête)
+link = './data/raw/point_arret.csv'
+data = np.genfromtxt(link, delimiter=',')[1:,:]
 
+# Enregistrer, dans un csv, toutes les coordonnées des points d'arrêts par trajet (i.e. par id_simulation) 
+id_simulation = np.unique(data[:,1])
 
+# Création et ouverture du fichier en mode écriture
+with open('data/steps.csv', mode='w') as csvFile:
+    # Créer un objet writer
+    writer = csv.writer(csvFile, delimiter=',')
 
+    for id in id_simulation :
+        i = 0
+        for step in data:
+            if step[1] == id :
+                if i==0 :
+                    trip = np.array([[step[-2], step[-1]]])
+                    i += 1
+                else:
+                    trip = np.append(trip, [np.array([step[-2], step[-1]])], axis=0)
+        writer.writerow([id, trip])
 
-# récupérer par id_simulation (i.e. par trajet) toutes les coordonnées des points d'arrêts dans l'ordre (i.e. par le paramètre index)
-
+csvFile.close()
 
