75013179c3cfc95c20818f453d195cf6ea7386bd
Claire_GRD <claire-girardin@hotmail.fr>
2023-03-29 23:36:24 +0200

    histo Claire

--- scripts/histogramme.py
+++ scripts/histogramme.py
@@ -1,5 +1,31 @@
 import numpy as np
-import matplotlib as plt 
+import matplotlib as plt
+import os
+import geopandas as gpd
+import pandas as pd
+import shapely
+from shapely.geometry import Point, LineString, shape
+import folium
+
+
+
+
+def create_df(filename) :
+    """
+    Create geodataframes from the simulation files
+
+    Args:
+        path_trajet (string): name of file in the directory ../data/raw
+
+    """
+    dirname = "data/raw/" # relative path to the gitignore directory - the function on the file _dataframes_gpd.py is adapted to find the relative path of this directory
+    df = pd.read_csv(dirname+filename,sep=';')
+
+    return df
+
+
+
+
 
 # Import CSV 
 
@@ -8,6 +34,7 @@ data = np.genfromtxt(link, delimiter=',')
 
 # Appel algo rayon 100km
 
+A = []
 
 plt.hist(A,bins=20,range=(0,4),color="y",edgecolor="gray",label="histogramme")
 plt.title('Histogramme de mutualisation des producteurs sur la France, dans un rayon de 100km autour des exploitations')
