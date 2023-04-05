ce41e13643d9260bd9ebc76fe6f5944901e5d5b5
Claire_GRD <claire-girardin@hotmail.fr>
2023-04-05 09:12:31 +0200

    histo fini

--- scripts/dataframes_gpd.py
+++ scripts/dataframes_gpd.py
@@ -2,13 +2,23 @@ import os
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point, LineString, shape
-import matplotlib
-import folium
 
-#import folium mapclassifypip 
-#import ast //pour le trajet
+
+# Script to create the CSV of the filtered and joined dataframes
+# Execute once to generate the csv files inside the folder "data/raw"
+#
+#
+
+
 
 root = os.path.join(os.path.dirname( __file__ ), os.pardir)  # relative path to the gitignore directory
+
+
+# we charge the shapes
+df_communes = gpd.read_file(root + "/data/assets/COMMUNE.shp")  # Layer IGN
+df_cheflieu = gpd.read_file(root + "/data/assets/CHFLIEU_COMMUNE.shp")  # Layer IGN
+df_france = gpd.read_file(root + "/data/assets/FRANCE.shp")  # Layer cree a partir du shape REGION de lIGN
+df_france = gpd.GeoDataFrame(df_france, geometry='geometry',crs="EPSG:2154")
 
 def create_simulation_df(filename) :
     """
@@ -63,47 +73,74 @@ def create_point_arret_df(filename) :
 
     return df
 
+
+
 #creation and filtering of utilisateurs dataframe
 df_utilisateurs = create_utilisateurs_df('utilisateurs.csv')
-df_utilisateurs_oui= df_utilisateurs[df_utilisateurs['prise en compte O/N ENSG']=='oui']
+df_utilisateurs= df_utilisateurs[df_utilisateurs['prise en compte O/N ENSG']=='oui']
 
 #creation of simulation dataframe and join with utilisateurs df
 df_simulation = create_simulation_df('simulation.csv')
-df_utilisateurs_simulations =pd.merge(df_simulation,df_utilisateurs_oui, left_on='id_utilisateur', right_on='id',how='inner')
+df_simulation_reel = df_simulation[df_simulation['type_simulation']=='reel']
 
 #creation of point_arret dataframe and join with utilisateurs_simulations
 df_point_arret = create_point_arret_df('point_arret.csv')
-df_simulations_arrets = pd.merge(df_point_arret,df_utilisateurs_simulations, left_on='id_simulation', right_on='id_x',how='inner')
 
-gdf_simulation_arrets= gpd.GeoDataFrame(df_simulations_arrets[['id_simulation','id_utilisateur']], geometry=gpd.points_from_xy(df_simulations_arrets.longitude, df_simulations_arrets.latitude))
 
-#join segment of lines
-geo_df = gdf_simulation_arrets.groupby(['id_simulation', 'id_utilisateur'],as_index=False)['geometry'].apply(lambda x: LineString(x.tolist()) if x.size > 1 else None)
+def create_geodataframe(simulation,utilisateur,point_arret):
 
-#set CRS and change to Lambert 2193
-geo_df = gpd.GeoDataFrame(geo_df, geometry='geometry',crs="EPSG:4326")
-geo_df= geo_df.to_crs("EPSG:2154")
+    """
+    Create geodataframe with all the necessary fields to run the algorithm to find the mutualisations
 
-#Remove invalid geometries- "lines" made of two identical points
-geo_df = geo_df[geo_df['geometry'].is_valid==True]
+    Args:
+        simulation, utilisateur, point_arret  (string): name of files in the directory ../data/raw
 
 
 
-df_communes = gpd.read_file(root + "/data/assets/COMMUNE.shp")  # Layer IGN
-df_cheflieu = gpd.read_file(root + "/data/assets/CHFLIEU_COMMUNE.shp")  # Layer IGN
+    """   
 
 
-geo_df['start']= geo_df['geometry'].apply(lambda x: Point(x.coords[0])) # we take the first point of the simmulation to verify where it is
-geo_df= gpd.GeoDataFrame(geo_df,geometry='start') #A Geodataframe can only have one geometry at a time. We change it to "Start" point to join it with the layer "communes"
-geo_df_commune = geo_df.sjoin(df_communes,how='left')
-geo_df_commune = geo_df_commune.drop(columns=['index_right']) #drop the column to do the jointure
-geo_df_chef_lieu = geo_df_commune.merge(df_cheflieu,left_on='ID', right_on='ID_COM') # attributaire join with the id of the cheflieu
-geo_df_chef_lieu = geo_df_chef_lieu.rename(columns ={'geometry_x':'intineraire','geometry_y':'cheflieu'})
+    print('start')
+    # we make the first dataframes    
+    df_utilisateurs_simulations =pd.merge(simulation,utilisateur, left_on='id_utilisateur', right_on='id',how='inner')
+    df_simulations_arrets = pd.merge(point_arret,df_utilisateurs_simulations, left_on='id_simulation', right_on='id_x',how='inner')
+    gdf_simulation_arrets= gpd.GeoDataFrame(df_simulations_arrets[['id_simulation','id_utilisateur']], geometry=gpd.points_from_xy(df_simulations_arrets.longitude, df_simulations_arrets.latitude))
+
+    #join segments of lines
+    geo_df = gdf_simulation_arrets.groupby(['id_simulation', 'id_utilisateur'],as_index=False)['geometry'].apply(lambda x: LineString(x.tolist()) if x.size > 1 else None)
+
+    #set CRS and change to Lambert 2193
+    geo_df = gpd.GeoDataFrame(geo_df, geometry='geometry',crs="EPSG:4326")
+    geo_df= geo_df.to_crs("EPSG:2154")
+
+    #Remove invalid geometries- "lines" made of two identical points
+    geo_df = geo_df[geo_df['geometry'].is_valid==True]
+      
+
+    geo_df['start']= geo_df['geometry'].apply(lambda x: Point(x.coords[0])) # we take the first point of the simmulation to verify where it is
+    geo_df= gpd.GeoDataFrame(geo_df,geometry='start') #A Geodataframe can only have one geometry at a time. We change it to "Start" point to join it with the layer "communes"
+  
+    geo_df_commune = geo_df.sjoin(df_communes,how='left')
+    geo_df_commune = geo_df_commune.drop(columns=['index_right']) #drop the column to do the jointure
+
+    
+    geo_df_chef_lieu = geo_df_commune.merge(df_cheflieu,left_on='ID', right_on='ID_COM') # attributaire join with the id of the cheflieu
+    geo_df_chef_lieu = geo_df_chef_lieu.rename(columns ={'geometry_x':'itineraire','geometry_y':'cheflieu'})
+
+    
+    geo_df_chef_lieu = gpd.GeoDataFrame(geo_df_chef_lieu,geometry='itineraire')
+
+
+    geo_df_chef_lieu = geo_df_chef_lieu.sjoin(df_france,how='left')
+    geo_df_chef_lieu = gpd.GeoDataFrame(geo_df_chef_lieu,geometry='start')
+    geo_df_chef_lieu = geo_df_chef_lieu.drop(columns=['index_right','ID_right','ID_left','NOM_M','NOM','INSEE_REG'])        
+    print(geo_df_chef_lieu.head())
+    print(geo_df_chef_lieu.geometry)
+    return geo_df_chef_lieu
 
 
 
-
-# le dataframe geo_df_chef_lieu est à enregistrer sous format csv
-# ajouter la condition de type_simulation (2 fichiers csv différents à créer)
-# ajouter le filtre de la France
-
+simulations_to_csv = create_geodataframe(df_simulation,df_utilisateurs,df_point_arret)
+simulations_reels_to_csv = create_geodataframe(df_simulation_reel,df_utilisateurs,df_point_arret)
+simulations_to_csv.to_csv(root + "/data/raw/" + "simulations_gdf.csv",sep=';', index=False)
+simulations_reels_to_csv.to_csv(root + "/data/raw/" + "simulations_reel_gdf.csv",sep=';', index=False)
--- scripts/histogramme.py
+++ scripts/histogramme.py
@@ -1,15 +1,15 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import os
 import geopandas as gpd
 import pandas as pd
 import shapely
 from shapely.geometry import Point, LineString, shape
 import folium
+from shapely import wkt
 
 
-filename = "_simulations_gdf.csv"
-# filename = "_simulations_reel_gdf.csv"
+
 
 def create_df(filename) :
     """
@@ -19,47 +19,56 @@ def create_df(filename) :
         path_trajet (string): name of file in the directory ../data/raw
 
     """
-    dirname = "data/raw/" # relative path to the gitignore directory - the function on the file _dataframes_gpd.py is adapted to find the relative path of this directory
-    df = pd.read_csv(dirname+filename,sep=';')
-
-    return df
-
-df = create_df(filename) # dataframe du fichier csv choisi
-
-dist = 100000
-
-tournee = df.iloc[[1]]
+    root = os.path.join(os.path.dirname( __file__ ), os.pardir)  # relative path to the gitignore directory
+    dirname = "/data/raw/" # relative path to the gitignore directory - the function on the file _dataframes_gpd.py is adapted to find the relative path of this directory
+    df = pd.read_csv(root +dirname+ filename,sep=';')
+    geometry = df['cheflieu'].map(wkt.loads)
+    gdf = gpd.GeoDataFrame(df, geometry=geometry, crs = 'EPSG:2154')
+    return gdf
 
 
-def IsIn(tournee, df, dist):
+def IsIn(gdf, dist):
+    n = gdf.count()[0] # number of total rounds
+    A = []
+    for i in range(n):
+        tournee = gdf.iloc[[i]]
+
+        geometry = tournee['cheflieu'].map(wkt.loads)
+        cheflieu = gpd.GeoDataFrame(tournee, geometry=geometry, crs = 'EPSG:2154') # we get the center of the cheflieu as a geometry
+        cheflieu['buffer'] = cheflieu.geometry.buffer(dist) # create the buffer based on the cheflieu point
+
+        cheflieu = cheflieu.drop(columns=['ID_COM']) # drop the column to do the jointure
+        cheflieu = gpd.GeoDataFrame(cheflieu, geometry='buffer') # we set the buffer as the gdf geometry
+
+        gdf['linestring'] = gdf['geometry'] # backup of linestring to preserve for the jointure
+        gdf = gpd.GeoDataFrame(gdf, geometry='geometry') #we set the line as a geometry
+
+        IsMutu = cheflieu.sjoin(gdf, predicate='contains', how='inner') # we join with lines from the simulations from before the manipulation
+        IsMutu = IsMutu[IsMutu['id_utilisateur_right']!=IsMutu['id_utilisateur_left']] # filter the users with the same ID
+
+        nbrMutu = IsMutu.count()[0]
+
+        A.append(nbrMutu)
+    
+    return A
 
 
-    cheflieu = gpd.GeoDataFrame(tournee, geometry='cheflieu') # we get the center of the cheflieu as a geometry
-    cheflieu['buffer'] = cheflieu.geometry.buffer(dist) # create the buffer based on the cheflieu point
-    cheflieu = cheflieu.drop(columns=['ID_COM','ID']) # drop the column to do the jointure
-    cheflieu = gpd.GeoDataFrame(cheflieu, geometry='buffer') # we set the buffer as the gdf geometry
-
-    df['linestring'] = df['geometry'] # backup of linestring to preserve for the jointure
-    df = gpd.GeoDataFrame(df, geometry='geometry') #we set the line as a geometry
-
-    IsMutu = cheflieu.sjoin(df, predicate='contains', how='inner') # we join with lines from the simulations from before the manipulation
-    IsMutu = IsMutu[IsMutu['id_utilisateur_right']!=IsMutu['id_utilisateur_left']] # filter the users with the same ID
-
-    return IsMutu
-
-
-print(IsIn(tournee, df, dist))
+def histo(data, filename, dist):
+    plt.hist(data,bins=10,color="blue",edgecolor="gray",label="histogramme") # 10 class histogram
+    plt.title('Histogramme du nombre de mutualisations possibles :'+filename)
+    plt.xlabel('Nombre de producteurs dans un rayon de '+str(dist*1e-3)+' km')
+    plt.ylabel('Fréquence')
+    plt.show()
 
 
 
-A = []
+if __name__ == "__main__":
+    # filename = "simulations_gdf.csv"
+    filename = "simulations_reel_gdf.csv"
 
+    gdf = create_df(filename) # dataframe du fichier csv choisi
+    dist = 100000
+    data = IsIn(gdf, dist)
 
-# utiliser seaborn plutôt que matplotlib
-
-# plt.hist(A,bins=20,range=(0,4),color="y",edgecolor="gray",label="histogramme")
-# plt.title('Histogramme de mutualisation des producteurs sur la France, dans un rayon de 100km autour des exploitations')
-# plt.xlabel('Nombre de producteur dans le rayon de 100 km')
-# plt.ylabel('Count')
-# plt.show()
+    histo(data, filename, dist)
 
