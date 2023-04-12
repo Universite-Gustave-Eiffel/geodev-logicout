380a0be48e0220394f16b643d99f5ea017be14dd
Sivloc <clovis.bergeret@laposte.net>
2023-04-12 09:43:01 +0200

    added comments

--- scripts/api_logicout.py
+++ scripts/api_logicout.py
@@ -15,10 +15,12 @@ def calcul_couts(traj):
         
         Takes an array of coordinates and create the json request for the Logicout API, then sends it and display the characteristics of 
     '''
+    #Parsing the coordinates array to build the json
     etapes = []
     for point in traj:
         etapes.append( {"longitude": point[0], "latitude": point[1], "duree_livraison": "00:10:00"})
     print(etapes)
+    #Building the json used for the request
     json_logicout = {
         'key': logicout_key,
         'id_vehicule': 'VUL_12',
@@ -35,15 +37,16 @@ def calcul_couts(traj):
     }
 
     data = json.dumps(json_logicout)
-
+    #Sending the json to the API
     r = requests.post(
         "https://www.logicout.fr/couts/api/calcul_parcours_detaille/",
         data=data,
         headers={'Content-Type': 'application/json'}
     )
     json_response = r.json()
+    #Printing
     print(json_response)
     
         
-test = ["48.8566,2.3522" , "51.5074,-0.1278" , "46.5468,1.6639"]
+test = [(48.8566,2.3522) , (1.5074,-0.1278) , (46.5468,1.6639)]
 calcul_couts(test)
