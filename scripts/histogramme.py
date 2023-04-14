f256f3eb81290d51480726a5a150a5129dfe59b6
Claire_GRD <claire-girardin@hotmail.fr>
2023-04-14 15:43:43 +0200

    maj isInclude

--- scripts/histogramme.py
+++ scripts/histogramme.py
@@ -3,25 +3,40 @@ import geopandas as gpd
 import pandas as pd
 import shapely
 from shapely.geometry import Point, LineString, shape
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
+import use_data
+import IsInclude
 
 
 
 
-def histo(data, filename, dist):
+def histo(gdf, typefile, dist, typeCheck):
+    """
+        typeCheck = 1 si une seule implication 
+                  = 2 si double implication
+
+        typefile = tournées quelconques ou réelles
+    """
     n = gdf.count()[0] # number of total rounds
     A = []
     for i in range(n):
         tournee = gdf.iloc[[i]]
+        if typeCheck == 1:
+            gdf_IsInclude = IsInclude.IsIn_tournee_gdf(tournee, gdf, dist)
+        else :
+            gdf_IsInclude = IsInclude.double_check(tournee, gdf, dist)
+        
+        nbrMutu = gdf_IsInclude.count()[0]
+        A.append(nbrMutu)
 
-
-
-    plt.hist(data,bins=20,color="blue",edgecolor="gray",label="histogramme") # 10 class histogram
-    plt.title('Histogramme du nombre de mutualisations possibles :'+filename)
-    plt.xlabel('Nombre de producteurs dans un rayon de '+str(dist*1e-3)+' km')
+    # affichage de l'histogramme
+    plt.hist(A,bins=20,color="blue",edgecolor="gray",label="histogramme")
+    plt.title('Histogramme du nombre de mutualisations possibles : tournées '+typefile)
+    plt.xlabel('Nombre de tournées dans un rayon de '+str(dist*1e-3)+' km pour des utilisateurs différents')
     plt.ylabel('Fréquence')
     plt.show()
+
 
 
 
@@ -29,9 +44,10 @@ if __name__ == "__main__":
     # filename = "simulations_gdf.csv"
     filename = "simulations_reel_gdf.csv"
 
-    gdf = create_df(filename) # dataframe du fichier csv choisi
-    dist = 50000
-    data = IsIn(gdf, dist)
-    print(gdf)
-    # histo(data, filename, dist)
+    gdf = use_data.create_gdf(filename) # dataframe du fichier csv choisi
+    dist = 100000
+    typeCheck = 2 # 1 ou 2
+    typefile = 'réelles' # 'quelconques' ou 'réelles'
+    print(histo(gdf, typefile, dist, typeCheck))
+
 
