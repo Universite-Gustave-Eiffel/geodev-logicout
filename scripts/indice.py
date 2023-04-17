e8d2f682bdd0bf7155c03500ccfb9e4a4e24e052
GautierTab <gautier.tabordet@gmail.com>
2023-04-17 10:20:00 +0200

    test indice 2

--- scripts/indice.py
+++ scripts/indice.py
@@ -7,27 +7,7 @@ import matplotlib as plt
 import numpy as np
 import folium
 from shapely import wkt
-
-
-
-def create_df(filename) :
-    """
-    Create geodataframes from the simulation files
-
-    Args:
-        path_trajet (string): name of file in the directory ../data/raw
-
-    """
-   
-    root = os.path.join(os.path.dirname( __file__ ), os.pardir)  # relative path to the gitignore directory
-    dirname = "/data/raw/" # relative path to the gitignore directory - the function on the file _dataframes_gpd.py is adapted to find the relative path of this directory
-    df = pd.read_csv(root +dirname+ filename,sep=';')
-    geometry = df['cheflieu'].map(wkt.loads)
-    gdf = gpd.GeoDataFrame(df, geometry=geometry, crs = 'EPSG:2154')
-    return gdf
-
-
-
+import use_data 
 
 
 
@@ -42,29 +22,28 @@ def distance(coupleA,coupleB):
 def indice(A, B, dist):
 
     aire = dist**2
+ 
+    A_df = gpd.GeoDataFrame(A, geometry=A['geometry'], crs = 'EPSG:2154')
+    B_df = gpd.GeoDataFrame(B, geometry=B['geometry'].map(wkt.loads), crs = 'EPSG:2154')
 
-    startA = pd.A['start']
-    startB = pd.B['start']
-    
-    # startA = A['itineraire'].apply(lambda x: Point(x.coords[0]))
-    # startB = B['itineraire'].apply(lambda x: Point(x.coords[0]))
-    # startA = [A[0][0],A[0][1]]
-    # startB = [B[0][0],B[0][1]]
+    startA = [A_df['start'],A_df['start']]
+    startB = [B_df['start'],B_df['start']]
 
     dist_start = distance(startA,startB)
     
     # Récupération de la liste des points de A
-    pointsA = A['itineaire']
+    pointsA = A_df['itineaire']
     # pointsA = gpd.GeoDataFrame(geometry=gpd.points_from_xy(A[:,0], A[:,1]))
     
     # Récupération de la liste des points de B
-    pointsB = B['itineaire']
+    pointsB = B_df['itineaire']
     # pointsB = gpd.GeoDataFrame(geometry=gpd.points_from_xy(B[:,0], B[:,1]))
     
     # Calcule la distance entre les points les plus éloignés entre les deux GeoDataFrames
     """
     Attention : à verifier si la distance est faite entre les points du coupleA et ceux du coupleB 2 à 2, ou alors 
     c'est le max entre tous les points du coupleA et tous les points du coupleB
+    --> Réponse : ok, distance entre tous les points
     """
     max_distance = pointsA.distance(pointsB).max()
     # A récupérer : l'indice des points A et B pour savoir entre quels points la distance est max
@@ -76,12 +55,20 @@ def indice(A, B, dist):
 
 if __name__ == "__main__":
 
+    gdf= use_data.create_gdf('simulations_reel_gdf.csv')
+    # gdf = gpd.GeoDataFrame(gdf, geometry=gdf['start'].map(wkt.loads),crs = 'EPSG:2154')
 
-    gdf= create_df('simulations_reel_gdf.csv')
-
-    # print(geo_df_envelop.crs)
-    
     A = gdf.iloc[0]
+    geometryA = A['cheflieu'].map(wkt.loads)
+    A = gpd.GeoDataFrame(A, geometry=geometryA, crs = 'EPSG:2154')
     B = gdf.iloc[1]
+
+    print(type(A['start']))
+    print('---------------------')
+    print(A)
+    print('---------------------')
+
+    
+
     ind = indice(A,B,100)
     print(ind)
