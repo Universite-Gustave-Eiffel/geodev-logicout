9861bf25cec0c6f4dbe5fbc61fc522cc0a7e4b81
TRabacal <thiago.carmuega-rabacal@ensg.eu>
2023-05-09 09:39:06 +0200

    avancement main

--- scripts/main.py
+++ scripts/main.py
@@ -2,12 +2,24 @@ import use_data
 import csv
 import pandas as pd
 import os
+import mutualisation
+
 root = os.path.join(os.path.dirname( __file__ ), os.pardir)  # relative path to the gitignore directory
 
 
-
-ranked_simulations = pd.read_csv(root + "/data/raw/ranked_mutualisations.csv")
 gdf = use_data.create_gdf('simulations_reel_gdf.csv', 'itineraire')
 
-gdf['coordinates'] = gdf.apply(lambda x : list(x.coords))
-print(gdf['coordinates'])
+
+with open('./data/raw/ranked_mutualisations.csv', mode='r') as file:
+    reader = csv.reader(file,delimiter=',')
+    next(reader)
+    empty_list=[]
+    for row in reader:
+        if(row[1]):
+            mutualisation.comparison(row[0],eval(row[1])[0][0],gdf)
+               
+        
+     
+print(empty_list)
+
+
