26d4a8c5203c878a95803e0c98333d70dd3a29da
Sivloc <clovis.bergeret@laposte.net>
2023-05-09 09:21:09 +0200

    added the producer's vehicle

--- scripts/api_logicout.py
+++ scripts/api_logicout.py
@@ -14,25 +14,25 @@ logicout_key = os.getenv('LOGICOUT_KEY')
 gmaps_key = os.getenv('GMAPS_KEY')
 mapbox_key = os.getenv('MAPBOX_SECRET_KEY')
 
-def calcul_couts(traj):
+def calcul_couts(traj,vehicule='VUL_12',v_type='GF',frigo=True,tps_moy='00:10:00',tps_act='00:00:00'):
     '''
         traj: Coordinates Array
         
         Takes an array of coordinates and create the json request for the Logicout API, then sends it and display the characteristics of 
     '''
+    #NB : The defaults values are chosen to match the worst case possible, so the script will give a pessimistic answer, but it cannot be worse in reality.
     #Parsing the coordinates array to build the json
     etapes = []
     for point in traj:
-        etapes.append( {"latitude": point[1], "longitude": point[0], "duree_livraison": "00:10:00"})
+        etapes.append( {"latitude": point[1], "longitude": point[0], "duree_livraison": tps_moy})
     print(etapes)
     #Building the json used for the request
     json_logicout = {
         'key': logicout_key,
-        'id_vehicule': 'VUL_12',
-        'type_vul': 'GF',
-        'frigorifique': True,
-        'duree_autres_activites': '01:30:00',
-        'heure_depart': '15/03/2025 15:48',
+        'id_vehicule': vehicule,
+        'type_vul': v_type,
+        'frigorifique': frigo,
+        'duree_autres_activites': tps_act,
         'service': 'mapbox',
         'retour': True,
         'etapes': etapes,
