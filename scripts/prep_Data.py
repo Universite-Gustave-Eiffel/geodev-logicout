1405c2672afeb4d11297c39d3c86ec0518d4fc79
TRabacal <thiago.carmuega-rabacal@ensg.eu>
2023-05-16 00:48:19 +0200

    maj prep_Data comments and last test

--- scripts/prep_Data.py
+++ scripts/prep_Data.py
@@ -12,6 +12,7 @@ root = os.path.join(os.path.dirname( __file__ ), os.pardir)  # relative path to 
 
 
 # we charge the shapefiles
+print('charging shapefiles ...')
 df_communes = gpd.read_file(root + "/data/assets/COMMUNE.shp")  # Layer IGN
 df_cheflieu = gpd.read_file(root + "/data/assets/CHFLIEU_COMMUNE.shp")  # Layer IGN
 df_france = gpd.read_file(root + "/data/assets/FRANCE.shp")  # Layer created from an IGN Shapefile
@@ -27,7 +28,7 @@ def create_simulation_df(filename) :
     """   
 
     
-    df = pd.read_csv(root +"/data/raw/"+ filename,sep=',')
+    df = pd.read_csv(root +"/data/raw/"+ filename,sep=';')
 
     return df
 
@@ -106,25 +107,32 @@ def create_geodataframe(simulation,utilisateur,point_arret):
         geo_df_cheflieu (geodataframe): panda's dataframe containing all data from the input file
 
     """  
-
+    #We set the progress bar to work with pandas
+    tqdm.pandas()
 
     print('start')
-    # we make the first dataframes
+    #we make the first dataframes
      
     #join between utilisateurs and simulations    
+    print('joining tables ...')
     df_utilisateurs_simulations =pd.merge(simulation,utilisateur, left_on='id_utilisateur', right_on='id',how='inner')
 
     #join with arrets and make a dataframe
+    
     df_simulations_arrets = pd.merge(point_arret,df_utilisateurs_simulations, left_on='id_simulation', right_on='id_x',how='inner')
     df_simulations_arrets= df_simulations_arrets.sort_values(by=['id_simulation','index'])
     gdf_simulation_arrets= gpd.GeoDataFrame(df_simulations_arrets[['id_simulation','id_utilisateur']], geometry=gpd.points_from_xy(df_simulations_arrets.longitude, df_simulations_arrets.latitude))
 
+  
     #join segments of lines
     geo_df = gdf_simulation_arrets.groupby(['id_simulation', 'id_utilisateur'],as_index=False)['geometry'].apply(lambda x: LineString(x.tolist()) if x.size > 1 else None)
 
+    print('transforming into geodataframes ...')
     #Setting the CRS and setting the projection to Lambert 2193
     geo_df = gpd.GeoDataFrame(geo_df, geometry='geometry',crs="EPSG:4326")
     geo_df= geo_df.to_crs("EPSG:2154")
+
+
 
     #Removing invalid geometries- "lines" made of two identical points
     geo_df = geo_df[geo_df['geometry'].is_valid==True]
@@ -135,15 +143,17 @@ def create_geodataframe(simulation,utilisateur,point_arret):
   
     geo_df_commune = geo_df.sjoin(df_communes,how='left')
     geo_df_commune = geo_df_commune.drop(columns=['index_right']) #Dropping the column to join effectively
-
-    geo_df_chef_lieu = geo_df_commune.merge(df_cheflieu,left_on='ID', right_on='ID_COM') # proerty based join with the id of the 'cheflieu'
+    
+    geo_df_chef_lieu = geo_df_commune.merge(df_cheflieu,left_on='ID', right_on='ID_COM') # property based join with the id of the 'cheflieu'
     geo_df_chef_lieu = geo_df_chef_lieu.rename(columns ={'geometry_x':'itineraire','geometry_y':'cheflieu'})
     geo_df_chef_lieu = gpd.GeoDataFrame(geo_df_chef_lieu,geometry='itineraire')
+  
 
     # Joining the chef_lieu dataframe onto the france dataframe
     geo_df_chef_lieu = geo_df_chef_lieu.sjoin(df_france,how='left')
     geo_df_chef_lieu = gpd.GeoDataFrame(geo_df_chef_lieu,geometry='start')
     
+
     #Cleaning the dataframe
     geo_df_chef_lieu = geo_df_chef_lieu.drop(columns=['index_right','ID_right','ID_left','NOM_M','NOM','INSEE_REG'])        
     print(geo_df_chef_lieu.head())
